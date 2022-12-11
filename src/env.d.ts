79a3ae6cd4ac9cc3a5209c20417b601aec5c273d
amjed-ali-k <amjedmgm@gmail.com>
2022-12-11 11:26:57 +0530

    init: bot repo

---
+++ src/env.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      GITHUB_AUTH_TOKEN: string;
+      NODE_ENV: "development" | "production";
+      PORT?: string;
+      PWD: string;
+      IG_USERNAME: string;
+      IG_PASSWORD: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
